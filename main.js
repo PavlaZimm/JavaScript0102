b9d25e852aba7353115466b7d69c01ee0fb7a397
Pavla Zimm <zimmermannovap@gmail.com>
2023-01-25 21:14:23 +0100

    ctverecek

---
+++ main.js
@@ -0,0 +1,37 @@
+// tady je místo pro náš program
+
+document.querySelector("#vysledek").innerHTML = secti(4, 5);
+
+/**
+ * Sečte dvě čísla na vstupu a vrátí výsledek
+ *
+ * @param {int} a
+ * @param {int} b
+ */
+function secti(a, b) {
+  let c = a + b;
+  return c;
+}
+
+/**
+ * Upozorní uživatele při spuštění.
+ */
+function upozorni() {
+  alert("Gratulace, právě jsi spustila tuto funkci!");
+}
+
+
+
+function zmenaBarvy(){
+  let ctverecek = document.querySelector('.ctverecek');
+  ctverecek.style.backgroundColor = "pink";
+}
+
+
+
+
+
+function secti(cislo1,cislo2){
+  let soucet = cislo1+cislo2;
+  return soucet;
+}
